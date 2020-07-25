8fde131db038d4809229d44ced80ef5efb6d4652
milanjovic6 <milanjovic6@gmail.com>
2020-07-25 12:38:29 +0200

    00

---
+++ 5-23.07.2020/script.js
@@ -0,0 +1,46 @@
+// Prvi zadatak
+
+let farenhajt = 232;
+let celzijus = 35;
+
+
+tempF = celzijus + 273.15;
+
+tempC = (farenhajt - 32) * 5/9;
+
+console.log(`Temperatura u Celzijusima iznosi ${tempC}`);
+
+console.log(`Temperatura u Farenhajtima iznosi ${tempF}`); 
+
+// Drugi zadatak
+
+//Broj poglavlja
+let n = 10;
+// Broj poglavlja prvog dana
+let a = 2;
+//Broj poglavlja drugog dana 
+let dd2 = a+2;
+ //Broj preostalih poglavlja
+let o = n-dd2-a;
+
+ console.log(`Broj prostalih poglavlja je ${o}`);
+
+ //Treci zadatak 
+
+ let p =800;
+
+ let m = 600;
+
+ let k = 400;
+ console.log(`Kusur iznosi ${k} rsd.`);
+ let dz=500;
+ console.log(`Cena dzempera ${dz} rsd.` );
+
+ let k1=p-dz;
+
+ let k2=k-k1;
+
+ console.log(`Perin kusur iznosi ${k1} rsd.`);
+ 
+ console.log(`Mikin kusur iznosi ${k2} rsd.`);
+ 
