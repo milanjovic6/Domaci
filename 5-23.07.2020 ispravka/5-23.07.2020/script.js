88c5c3bc00b6368700a608a08ef33a9778abd34f
milanjovic6 <68284131+milanjovic6@users.noreply.github.com>
2020-07-29 00:34:04 +0200

    Add files via upload

---
+++ 5-23.07.2020 ispravka/5-23.07.2020/script.js
@@ -0,0 +1,54 @@
+// Prvi zadatak
+
+let farenhajt = 80;
+
+var tempC = (farenhajt - 32) * 5/9;
+
+var tempK = tempC + 273.15;
+
+let kelvin=300;
+
+tempC = kelvin -273.15;
+let tempF = tempC * 9/5 + 32;
+//var tempF = kelvin * 9/5 -459.67;
+
+
+console.log(`Temperatura u Celzijusima iznosi ${tempC}`);
+
+console.log(`Temperatura u Kelvinima iznosi ${tempK}`); 
+
+console.log(`Temperatura u Farenhajtima iznosi ${tempF}`); 
+
+
+// Drugi zadatak
+
+//Broj poglavlja
+let n = 10;
+// Broj poglavlja prvog dana
+let a = 2;
+//Broj poglavlja drugog dana 
+let dd2 = a+2;
+ //Broj preostalih poglavlja
+let o = n-dd2-a;
+
+ console.log(`Broj prostalih poglavlja je ${o}`);
+
+ //Treci zadatak 
+
+ let p =1000;
+
+ let m = 600;
+
+ let k = 600;
+ console.log(`Kusur iznosi ${k} rsd.`);
+ let dz=(p+m-k)/2;
+ console.log(`Cena dzempera ${dz} rsd.`);
+
+ let k1=p-dz;
+
+ let k2=k-k1;
+
+ console.log(`Perin kusur iznosi ${k1} rsd.`);
+ 
+ console.log(`Mikin kusur iznosi ${k2} rsd.`);
+ 
